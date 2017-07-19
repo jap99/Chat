9ee8b8d23cc04fe63cbfc92ee26199e96573c2ba
javidp.sf@gmail.com <javidpoornasir@Javids-MacBook-Pro.local>
2017-07-18 19:38:41 -0700

    Implement SWRevealViewController

---
+++ chatapp/Controller/ChannelVC.swift
@@ -0,0 +1,21 @@
+//
+//  ChannelVC.swift
+//  chatapp
+//
+//  Created by Javid Poornasir on 7/18/17.
+//  Copyright © 2017 Javid Poornasir. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60 
+    }
+
+    
+
+}
---
+++ chatapp/Controller/ChatVC.swift
@@ -0,0 +1,32 @@
+//
+//  ChatVC.swift
+//  chatapp
+//
+//  Created by Javid Poornasir on 7/18/17.
+//  Copyright © 2017 Javid Poornasir. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+   
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+
+  
+    
+    
+    
+    
+    
+
+}
