76dcd5fe218e26dc465818bbee132e8712ff58c8
javidp.sf@gmail.com <javidpoornasir@Javids-MacBook-Pro.local>
2017-07-24 12:02:29 -0700

    send messages

--- chatapp/Controller/ChatVC.swift
+++ chatapp/Controller/ChatVC.swift
@@ -12,11 +12,15 @@ class ChatVC: UIViewController {
 
     @IBOutlet weak var menuBtn: UIButton!
     @IBOutlet weak var channelNameLbl: UILabel!
+    @IBOutlet weak var messageTxtBox: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.bindToKeyboard()
+        let tap = UITapGestureRecognizer(target: self, action: #selector(ChatVC.handleTap))
+        view.addGestureRecognizer(tap)
         menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
@@ -37,7 +41,9 @@ class ChatVC: UIViewController {
         updateWithChannel()
     }
     
-    
+    @objc func handleTap() {
+        view.endEditing(true)
+    }
     
     func updateWithChannel() {
         let channelName = MessageService.instance.selectedChannel?.channelTitle ?? ""
@@ -54,6 +60,23 @@ class ChatVC: UIViewController {
             channelNameLbl.text = "Please log in"
         }
     }
+    
+    
+    
+    @IBAction func sendButtonPressed(_ sender: Any) {
+        if AuthService.instance.isLoggedIn {
+            guard let channelId = MessageService.instance.selectedChannel?.id else { return }
+            guard let message = messageTxtBox.text else { return }
+            
+            SocketService.instance.addMessage(messageBody: message, userId: UserDataService.instance.id, channelId: channelId, completion: { (success) in
+                if success {
+                    self.messageTxtBox.text = ""
+                    self.messageTxtBox.resignFirstResponder()
+                }
+            })
+        }
+    }
+    
     
     
     
---
+++ chatapp/View/KeyboardBoundView.swift
@@ -0,0 +1,37 @@
+//
+//  KeyboardBoundView.swift
+//  chatapp
+//
+//  Created by Javid Poornasir on 7/24/17.
+//  Copyright © 2017 Javid Poornasir. All rights reserved.
+//
+
+import Foundation
+
+extension UIView {
+    
+    func bindToKeyboard() {
+        NotificationCenter.default.addObserver(self, selector: #selector(UIView.keyboardWillChange(_:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+        
+    }
+    
+    
+    @objc func keyboardWillChange(_ notification: NSNotification) {
+        
+        let duration = notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! Double
+        let curve = notification.userInfo![UIKeyboardAnimationCurveUserInfoKey] as! UInt
+        let curFrame = (notification.userInfo![UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        let targetFrame = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let deltaY = targetFrame.origin.y - curFrame.origin.y
+        
+        UIView.animateKeyframes(withDuration: duration, delay: 0.0, options: UIViewKeyframeAnimationOptions(rawValue: curve), animations: {
+            self.frame.origin.y += deltaY
+            
+        }, completion: { (true) in
+            self.layoutIfNeeded()
+        })
+        
+    }
+    
+    
+}
